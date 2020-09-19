a3c3208e1cab2377ae2368e3a9da6f359bd927d1
tiffchannn <tiffany.yi.chan@gmail.com>
2020-09-18 17:31:47 -0700

    Working on bringing in mongo

--- database/index.js
+++ database/index.js
@@ -1,8 +1,27 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  id: Number,
+  name: String,
+  owner: {
+    login: String,
+    id: Number,
+    avatar_url: String,
+    html_url: String,
+    repos_url: String
+  },
+  stargazers_count: Number,
+  watchers_count: Number,
+  forks_count: Number
+
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
