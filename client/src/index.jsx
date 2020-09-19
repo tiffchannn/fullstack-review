7dc5e92cfa52bea25aed894584e1d8f785cbe638
tiffchannn <tiffany.yi.chan@gmail.com>
2020-09-18 17:31:22 -0700

    Added event handlers and event listeners

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import $ from 'jquery';
 import Search from './components/Search.jsx';
 import RepoList from './components/RepoList.jsx';
@@ -7,22 +8,53 @@ import RepoList from './components/RepoList.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      repos: []
+    this.state = {
+      repos: [],
+      term: ''
     }
 
+    this.onChange = this.onChange.bind(this);
+    this.search = this.search.bind(this);
+    this.getRepoGitHub = this.getRepoGitHub.bind(this);
   }
 
-  search (term) {
-    console.log(`${term} was searched`);
-    // TODO
+  componentDidMount() {
+    this.getRepoGitHub()
+  }
+
+  getRepoGitHub() {
+    axios.get('/repos')
+      .then((data) => {
+        this.setState({
+          term: data.data
+        })
+      }, () => console.log("username github: ", this.state.term))
+      .catch(err => console.log(err))
+  }
+
+  onChange (e) {
+    this.setState({
+      term: e.target.value
+    }, () => console.log(this.state.term));
+  }
+
+  search (e) {
+     e.preventDefault();
+     axios.post('/repos', {term: this.state.term})
+       .then(() => this.getRepoGitHub())
+       .catch(err => console.log(err))
+    console.log(`${this.state.term} was searched`);
+
   }
 
   render () {
     return (<div>
       <h1>Github Fetcher</h1>
       <RepoList repos={this.state.repos}/>
-      <Search onSearch={this.search.bind(this)}/>
+      {/* <Search onSearch={this.search.bind(this)}/> */}
+      <input value={this.state.term} onChange={this.onChange}></input>
+      <h4>Add more repos!</h4>
+      <button>Add Repos!</button>
     </div>)
   }
 }
