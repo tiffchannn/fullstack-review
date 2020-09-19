480921d8e7b8568dd76168be4c0a29c72a814e1d
tiffchannn <tiffany.yi.chan@gmail.com>
2020-09-18 17:32:27 -0700

    Working on adding get and post request functionality

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+// const router = require('./router');
+
 let app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.use(express.static(__dirname + '/../client/dist'));
 
